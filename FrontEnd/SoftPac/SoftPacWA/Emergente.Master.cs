39e41f7652870a7a54b1a5d8ba83d06e6098fd23
EgoidGarnet <a20233483@pucp.edu.pe>
2025-11-14 09:12:08 -0500

    v4

---
+++ FrontEnd/SoftPac/SoftPacWA/Emergente.Master.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web.UI;
+
+namespace SoftPacWA
+{
+    public partial class Emergente : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
